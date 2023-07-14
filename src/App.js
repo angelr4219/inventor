6cb36c2cf000e521e7fc8d918962b0d43d690c92
angelr319 <139201214+angelr4219@users.noreply.github.com>
2023-07-13 22:30:10 -0700

    This is the start of me learning react for javascript html and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+       <Start />
+       <Info />
     </div>
+  );
+}
+
+function Start(){
+ const title = "this is it";
+ const is = false;
+ 
+  return(
+    <div>
+      <h1>
+    
+      </h1>
+    </div>
+  );
+}
+
+
+
+
+function Info() {
+  return (
+    <form>
+      <h1>Inventory system</h1>
+      <p>Managing things</p>
+      <label for = "text-form"> Please put an input</label>
+      <input type = "text"/>
+    </form>
+      
+  
   );
 }
 
